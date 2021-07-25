0444ad89106e09bbe2bef115a9e9bc6b9c351552
Thomas Fan <thomasfan100@gmail.com>
2021-07-25 11:15:37 -0500

    mod3

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,33 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, g
+import sqlite3
+
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+def open_connection():
+    connection = getattr(g,'_connection',None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factor = sqlite3.Row
+    return connection
+
+def execute_sql(sql,values=(), commit=False,singleFalse):
+    connection = open_connection()
+    cursor = connection.execute(sql,values)
+    if commit==True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single lese cursor.fetchall()
+    curso.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g,'_connection',None)
+    if connection is not None:
+        connection.close()
+        
 @app.route('/')
 @app.route('/jobs')
 def jobs():
